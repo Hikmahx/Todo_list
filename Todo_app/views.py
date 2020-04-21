1c8749c7d52dc8c33eb8900f3c90fd79df78eb62
Hikmahx <hikmayousuph@gmail.com>
2020-04-20 23:14:28 -0700

    added details to views.py

--- Todo_app/views.py
+++ Todo_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.models import Todo
+from .models import Todo
 
 # Create your views here.
 def index(request):
     #return HttpResponse("it is working!")
-    listed = Todo.objects.get()
+    listed = Todo.objects.all()[:10]
     context ={
-        "text" : "text",
+        "text" : "To do items",
         "listed": listed
     }
     return render(request, 'Todo_app/index.html', context)
+
+def details(request, id):
+    items = Todo.objects.get(id=id)
+
+    context = {
+        'items': items
+    }
+    return render(request, 'Todo_app/details.html', context)
