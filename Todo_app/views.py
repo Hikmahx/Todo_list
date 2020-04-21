6461e5a4dc168ea7d496933b7fbd080c5fdb6612
Hikmahx <hikmayousuph@gmail.com>
2020-04-20 22:47:01 -0700

    modified index request, added template folder

--- Todo_app/views.py
+++ Todo_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.models import Todo
+
 # Create your views here.
 def index(request):
-    return HttpResponse("it is working!")
+    #return HttpResponse("it is working!")
+    listed = Todo.objects.get()
+    context ={
+        "text" : "text",
+        "listed": listed
+    }
+    return render(request, 'Todo_app/index.html', context)
